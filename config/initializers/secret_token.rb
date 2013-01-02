891a3e83c182ccc020f6f3c3e1c42cf97066834f
alexia <la002769@etu.unice.fr>
2013-01-02 16:49:05 +0100

    Added rspec support

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetApp::Application.config.secret_token = 'f33082762db1e7ca16ce0260481415a93776e1de0c3f204804a07c0d13b2561d71d2c2992b454a500001b17606e39965dae98370658a5a0494b7c68326643ba2'
