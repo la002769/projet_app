f0c25ac7f7b4b1930bbb04f910eac9681e1d87ed
alexia <la002769@etu.unice.fr>
2013-01-13 22:20:52 +0100

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,11 +9,17 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Projet App')
     end
 
-    it "should have the title 'Home'" do
+ it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Projet App | Home")
+                        :text => "Ruby on Rails Tutorial Projet App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+    
   end
 
   describe "Help page" do
@@ -22,11 +28,18 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
- it "should have the title 'Help'" do
+ 
+  it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Projet App | Help")
+                        :text => "Ruby on Rails Tutorial Projet App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title', :text => '| Help')
+    end
+ 
   end
 
   describe "About page" do
@@ -36,10 +49,16 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'About')
     end
 
-    it "should have the title 'About'" do
+ it "should have the base title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Projet App | About")
+                        :text => "Ruby on Rails Tutorial Projet App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/about'
+      page.should_not have_selector('title', :text => '| About')
+    end
+
   end
 end
